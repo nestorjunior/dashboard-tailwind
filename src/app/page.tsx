f8c59af5aada830787a0512c153cf311089f61fd
Nestor Junior <contato.nestor@gmail.com>
2024-08-26 10:05:06 -0300

    feat: abas form animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { SettingsTab } from "./components/SettingsTab";
 
 export default function Home() {
   return (
     <>
-      <h2>Dashboard</h2>
+      <h2 className="text-3xl font-medium text-zinc-900">Settings</h2>
+      <SettingsTab />
     </>
   );
 }
